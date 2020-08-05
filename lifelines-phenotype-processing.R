e11a2bf733e94e625c373f5189365504e1b964c4
Robert Warmerdam <robert_warmerdam@hotmail.com>
2020-08-05 13:34:32 +0200

    Added phenotype source map

--- lifelines-phenotype-processing.R
+++ lifelines-phenotype-processing.R
@@ -15,7 +15,7 @@ library(tidyverse)
 library(argparse)
 
 ##############################
-# Define argument parser
+# Get the path to the default phenotype source map
 ##############################
 
 initial.options <- commandArgs(trailingOnly = FALSE)
@@ -23,12 +23,19 @@ file.arg.name <- "--file="
 script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
 script.basename <- dirname(script.name)
 default.phenotype_sources_map <- file.path(script.basename, "phenotype-sources-map.txt")
+if (length(default.phenotype_sources_map) == 0) {
+  default.phenotype_sources_map = NULL
+}
+
+##############################
+# Define argument parser
+##############################
 
 parser <- ArgumentParser(description='')
-parser$add_argument('--phenotypes_base_path',
+parser$add_argument('--phenotypes-base-path',
                     help='path to directory where tab-separated Lifelines phenotypes are stored')
 
-parser$add_argument('--phenotype_source_map', required = FALSE, 
+parser$add_argument('--phenotype-source-map', required = FALSE, 
                     default = default.phenotype_sources_map,
                     help=paste0('tab-separated file with header and colomns indicating the phenotype name,',
                                 'the column identifier and the source file.'))
@@ -44,7 +51,9 @@ parser$add_argument('--out',
 # Define functions
 ##############################
 
-
+getHeight <- function() {
+  
+}
 
 ##############################
 # Generate phenotypes
@@ -52,5 +61,63 @@ parser$add_argument('--out',
 
 args <- parser$parse_args(commandArgs(trailingOnly = TRUE))
 
-print(args)
-# args <- parser$parse_args(c("--phenotypes_base_path", ""))
+args <- parser$parse_args(c(
+  "--phenotypes-base-path", "/groups/umcg-lifelines/tmp01/projects/phenotypes/tab_seperated_labels/",
+  "--gsa-linkage-file", "/groups/umcg-lifelines/tmp01/releases/gsa_linkage_files/v1/gsa_linkage_file.dat",
+  "--out", "/groups/umcg-lifelines/tmp01/projects/ugli_blood_gsa/pgs_based_mixup_correction/data/lifelines/processed/UGLI.pgs.phenotypes.dat",
+  "--phenotype-source-map", "/home/umcg-rwarmerdam/pgs_based_mixup_correction/scripts/r-scripts/pgs_based_sample_mix-up_correction/phenotype-source-map.txt"))
+
+# Collect required columns
+
+# Height
+
+
+# BMI
+
+# Diastolic blood pressure
+
+# Systolic blood pressure
+
+# Estimated GFR
+
+# Basophil concentration
+
+# Eosinophil concentration
+
+# HbA1c concentration
+
+# LDL-cholesterol
+
+# Log-transformed triglyceride concentration
+
+# Square root of HDL-cholesterol
+
+# Total cholesterol concentration
+
+# Hematocrit concentration
+
+# Hemoglobin concentration
+
+# Lymphocyte concentration
+
+# Monocyte concentration
+
+# Reticulocyte concentration
+
+# Neutrophil concentration
+
+# Thrombocyte concentration
+
+# Type-2 diabetes
+
+# Type-1 diabetes
+
+# Asthma
+
+# Hair colour (level of blonde (black - blonde))
+
+# Hair colour (red vs. a level of blonde (black - blonde))
+
+# Coronary artery disease
+
+# Schizophrenia
