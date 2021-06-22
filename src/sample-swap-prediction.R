e7f9606ef6746ee96a4c06bbde1dd2fbd6622aef
Robert Warmerdam <robert_warmerdam@hotmail.com>
2021-06-22 21:25:29 +0200

    Only using 10000 samples

--- src/sample-swap-prediction.R
+++ src/sample-swap-prediction.R
@@ -969,12 +969,14 @@ phenotypesFilePath <- args$phenotypes_file
 phenotypesTable <- fread(phenotypesFilePath, header=T, quote="", sep="\t") %>%
   distinct() %>%
   rename_all(recode, "UGLI_ID" = "ID") %>%
-  mutate(SEX = factor(SEX, levels = c("Female", "Male"))) %>%
+  mutate(SEX = case_when(SEX == 1 ~ "Female", SEX == 2 ~ "Male", TRUE ~ SEX),
+         SEX = factor(SEX, levels = c("Female", "Male"))) %>%
   group_by(ID) %>%
   filter(!any(AGE < 18)) %>%
   ungroup()
 
-link <- data.frame(geno = unique(phenotypesTable$ID), pheno = unique(phenotypesTable$ID), stringsAsFactors = F)
+link <- data.frame(geno = unique(phenotypesTable$ID), pheno = unique(phenotypesTable$ID), stringsAsFactors = F) %>%
+  slice_sample(10000)
 
 # Get the link path
 if (!is.null(args$sample_coupling_file)) {
